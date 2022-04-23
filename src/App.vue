1b5d9f72affb0c21e3a85a176725b0ad6952179b
Jin-Woo Jeong <jinw.jeong@seoultech.ac.kr>
2022-04-23 20:25:38 +0900

    Added tf model demo

--- src/App.vue
+++ src/App.vue
@@ -13,40 +13,34 @@
       </div>
 
       <v-spacer></v-spacer>
-
+      <v-btn @click="predict"> Detect </v-btn>
       <v-btn text>
         <span>More</span>
-        <v-menu
-            bottom
-            left
-          >
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                dark
-                icon
-                v-bind="attrs"
-                v-on="on"
-              >
-                <v-icon>mdi-dots-vertical</v-icon>
-              </v-btn>
-            </template>
+        <v-menu bottom left>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn dark icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
 
-            <v-list>
-              <v-list-item
-                v-for="(item, i) in items"
-                :key="i"
-                @click=deviceFeature(item.title)
-              >
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
+          <v-list>
+            <v-list-item
+              v-for="(item, i) in items"
+              :key="i"
+              @click="deviceFeature(item.title)"
+            >
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
         <!-- <v-icon>mdi-menu</v-icon> -->
       </v-btn>
     </v-app-bar>
 
     <v-main>
-      <TodoHeader :msg=message />
+      <v-file-input v-model="files" />
+      <img id="img" :src="src" width="100%" />
+      <TodoHeader :msg="message" />
       <TodoInput v-on:addTodo="addTodo"></TodoInput>
       <TodoList
         v-bind:propsdata="todoItems"
@@ -64,6 +58,9 @@ import TodoFooter from "./components/TodoFooter.vue";
 import TodoHeader from "./components/TodoHeader.vue";
 import TodoList from "./components/TodoList.vue";
 import TodoInput from "./components/TodoInput.vue";
+import * as tf from "@tensorflow/tfjs-core";
+import "@tensorflow/tfjs-backend-cpu";
+import * as cocoSSD from "@tensorflow-models/coco-ssd";
 
 export default {
   name: "App",
@@ -72,7 +69,7 @@ export default {
     TodoList,
     TodoFooter,
     TodoHeader,
-    TodoInput
+    TodoInput,
   },
 
   data() {
@@ -83,8 +80,11 @@ export default {
         { title: "battery" },
         { title: "geo" },
         { title: "network" },
-        { title: "vibrate" }
-      ]
+        { title: "vibrate" },
+      ],
+      src: "",
+      model: "",
+      files: null,
     };
   },
   methods: {
@@ -111,7 +111,7 @@ export default {
           this.message = "Network status:" + navigator.connection.type;
           break;
         case "battery":
-          navigator.getBattery().then(battery => {
+          navigator.getBattery().then((battery) => {
             this.message = "Battery status:" + battery.level;
             battery.onchargingchange = () => {
               this.addTodo(battery.charging);
@@ -119,11 +119,20 @@ export default {
           });
           break;
         case "geo":
-            navigator.geolocation.getCurrentPosition(location => {
-              this.message = "Current Position:" + location.coords.longitude+"&"+location.coords.latitude;
-            })
+          navigator.geolocation.getCurrentPosition((location) => {
+            this.message =
+              "Current Position:" +
+              location.coords.longitude +
+              "&" +
+              location.coords.latitude;
+          });
       }
-    }
+    },
+    async predict() {
+      const img = document.getElementById("img");
+      let tmp = await this.model.detect(img);
+      tmp.forEach(obj => this.message += " "+obj.class)
+    },
   },
   created() {
     if (localStorage.length > 0) {
@@ -131,7 +140,26 @@ export default {
         this.todoItems.push(localStorage.key(i));
       }
     }
-  }
+  },
+  async mounted() {
+    this.model = await cocoSSD.load();
+
+    console.log("model loaded");
+  },
+  watch: {
+    files() {
+      console.log(this.files);
+      var reader = new FileReader();
+      // Define a callback function to run, when FileReader finishes its job
+      reader.onload = (e) => {
+        // Note: arrow function used here, so that "this.imageData" refers to the imageData of Vue component
+        // Read image as base64 and set to imageData
+        this.src = e.target.result;
+      };
+      // Start the reader job - read file as a data url (base64 format)
+      reader.readAsDataURL(this.files);
+    },
+  },
 };
 </script>
 
